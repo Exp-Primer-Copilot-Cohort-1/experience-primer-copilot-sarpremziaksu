6e71a358b9d4018f1b492efcfd364a8eb6f5e95e
Sarp Remzi AKSU <94607857+sarpremziaksu@users.noreply.github.com>
2023-09-18 17:08:10 +0300

    Update members.js
    malaka

--- members.js
+++ members.js
@@ -1,1 +1,8 @@
 function skillsMember()
+{
+    var skills = document.getElementById("skills");
+    var skill = document.createElement("div");
+    skill.className = "skill";
+    skill.innerHTML = '<input type="text" name="skills[]" placeholder="Skill" required><input type="text" name="skillLevel[]" placeholder="Skill Level" required><input type="button" value="Remove" onclick="removeSkill(this)">';
+    skills.appendChild(skill);
+}
